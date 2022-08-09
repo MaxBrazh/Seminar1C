01cecc38d7129eb00fcd269915c580181e6482ea
Maksim <brazhmax@mail.ru>
2022-08-09 15:24:32 +0300

    add Zadacha 6

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 6
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+
+if ((a % 2) == 0)
+{
+Console.WriteLine("Число a чётное");
+}
+else
+{
+Console.WriteLine("Число а нечётное");  
+}
