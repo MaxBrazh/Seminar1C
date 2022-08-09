db434932b0cde6239511cc4b01bebc61b2b43602
Maksim <brazhmax@mail.ru>
2022-08-09 15:21:58 +0300

    add Zadacha 4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,14 @@
+﻿///Задача 4
+Console.WriteLine("Введите значение a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение c: ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
