c99d2c89f39d37cdc8bf5c576f114e0f8ead50c1
Maksim <brazhmax@mail.ru>
2022-08-09 15:28:28 +0300

    add Zadacha 8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 8
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= N)
+    {
+    if ((i % 2) == 0)
+        {
+        if (i != 2)
+            Console.Write(",");
+
+        Console.Write(i);
+        }
+    i++;
+    }
