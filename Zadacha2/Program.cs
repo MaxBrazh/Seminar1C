964e2ef062ff948977c61b7879b9810ce132662f
Maksim <brazhmax@mail.ru>
2022-08-09 15:33:14 +0300

    add Zadacha 2 full

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2
+Console.WriteLine("Введите значение a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b: ");
+int b = int.Parse(Console.ReadLine());
+if (a < b)
+{
+Console.WriteLine("max = b");
+Console.WriteLine("min = a");
+}
+if (a > b)
+{
+Console.WriteLine("max = a");
+Console.WriteLine("min = b");
+}
